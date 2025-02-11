85e21b01ad20672266c8dce659d0845c721364f1
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-11 22:00:19 +0800

    update

---
+++ home/js/script.js
@@ -0,0 +1,7 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var content = document.getElementById("content");
+    
+    sidebar.classList.toggle("active");
+    content.classList.toggle("active");
+}
